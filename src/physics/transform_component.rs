0a8c5fa0f58169bc9812989f9fe9b2f546698b28
Max Erdelmeier <erdelmei@rhrk.uni-kl.de>
2022-01-03 11:37:26 +0100

    Hello TURNING Triangle

--- src/physics/transform_component.rs
+++ src/physics/transform_component.rs
@@ -1,4 +1,4 @@
-use cgmath::{Matrix4, SquareMatrix, Vector3};
+use cgmath::{Angle, Deg, Matrix4, Rad, SquareMatrix, Vector3};
 
 pub struct TransformComponent {
     transform: Matrix4<f32>
@@ -19,5 +19,9 @@ impl TransformComponent {
 
     pub fn translate(&mut self, shift: Vector3<f32>) {
         self.transform = Matrix4::from_translation(shift) * self.transform;
+    }
+
+    pub fn rotate(&mut self, angle: f32) {
+        self.transform = Matrix4::from_angle_y(Deg::asin(angle)) * self.transform;
     }
 }
