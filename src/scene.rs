538c9de35bdbe6ad4de090977961cadfaf9508f0
Max Erdelmeier <erdelmei@rhrk.uni-kl.de>
2022-10-26 13:49:01 +0200

    Stop using mod.rs

--- src/scene.rs
+++ src/scene.rs
@@ -1,5 +1,10 @@
-use crate::ui::Event;
+mod menu_scene;
+mod test_scene;
+
 use std::time::Duration;
+pub use menu_scene::MenuScene;
+pub use test_scene::TestScene;
+use crate::ui::Event;
 
 pub trait Scene {
     fn update(&mut self, dt: &Duration) -> Option<Box<dyn Scene>>;
--- src/scene/mod.rs
+++
@@ -1,7 +0,0 @@
-mod menu_scene;
-mod scene;
-mod test_scene;
-
-pub use menu_scene::MenuScene;
-pub use scene::Scene;
-pub use test_scene::TestScene;
